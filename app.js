f5add63e391290cd1f93ebc20ecc7db5db5b5d7f
Eunbyul <las4134@gmail.com>
2023-03-10 08:07:31 +0000

    add generateHTML function

--- app.js
+++ app.js
@@ -15,9 +15,29 @@ async function fetchAPI(){
   let baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_key}&to=20`;
   let response = await fetch(baseURL);
   let data = await response.json()
-   generateHTML(data.hits)
+  //console.log(data)
+  generatedHTML(data.hits)
 }
 
-function generateHTML(results){
-  console.log(results)
+function generatedHTML(results){
+console.log(results)
+
+let generateCard = '';
+
+results.map( (result) => {
+  generateCard += 
+
+  ` <div class="item">
+  <img src="${result.recipe.image}" alt="">
+  <div class="flex-container">
+    <h1 class="title">${result.recipe.label}</h1>
+    <a class="view-btn" href="${result.recipe.url}" target="_blank">View Recipe</a>
+  </div>
+  <p class="item-data">Calories: ${Math.floor(result.recipe.calories)}</p>
+</div> 
+  `
+});
+
+searchResultDiv.innerHTML = generateCard;
+
 }
